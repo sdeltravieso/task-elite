940f98994ae2e556b0fcd5406b7c5338de8a23e1
jkrowden <jkrowden@gmail.com>
2018-07-12 20:19:41 -0600

    Building for production

--- client/src/Components/Dashboard/MainContent/MainContent.js
+++ client/src/Components/Dashboard/MainContent/MainContent.js
@@ -4,7 +4,7 @@ import Departments from "./Departments";
 import MainTasks from "./MainTasks";
 import CompletedTasks from "./CompletedTasks";
 import "./MainContent.css";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 const MainContent = (props) => (
 
@@ -18,11 +18,12 @@ const MainContent = (props) => (
 		{/*This is where the react router will decide which component to load*/}
 		{/* <Users /> */}
 		{/* <Departments /> */}
+		<Switch>
 			<Route path="/tasks" component={MainTasks}/>
 			<Route path="/users" component={Users}/>
 			<Route path="/department" component={Departments}/>
 			<Route path="/completedTasks" component={CompletedTasks}/>
-
+	</Switch>
 	</div>
 
 )
